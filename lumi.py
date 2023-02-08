Correct NN calculation to incorporate missing ings

lumi.py
@@ -6,6 +6,8 @@ from lib import objs
 
 pd.options.display.width = 10
 
+folder_fp = env_folder + "files/"
+
 def categorize_ingredient(ingredient):
     if type(ingredient) != str:
         return "Undefined"
@@ -31,7 +33,7 @@ def database_to_df(db_filepath):
 
     # Separate out ingredient metadata to merge later
     db_ings = db_recipe.iloc[6:, :6].drop(2, axis=1)
-    db_ings[0] = list(map(lambda x: str(x).strip(), db_ings[0]))
+    db_ings[0] = db_ings[0].str.strip()
     db_ings.columns = ["Function", "Sub-Group", "Ingredient", "Ing_PLM", "Ing_Description"]
     db_ings.insert(0, "Ing_ID", db_ings.index)
 
@@ -46,13 +48,14 @@ def database_to_df(db_filepath):
     )
 
     db_recipe.columns = cols
-    db_recipe = db_recipe.drop(0).dropna(subset=["Ing_pct"])
+    db_recipe = db_recipe.drop(0)
     db_recipe.dropna(how="all", subset=["Request", "Batch"], inplace=True)
+    db_recipe["Ing_pct"].fillna(0, inplace=True)
 
     # Add ingredient information back in
     db_recipe = db_recipe.merge(db_ings, on="Ing_ID", how="left")
     for column in cols[:-2]:
-        db_recipe[column] = list(map(lambda x: str(x).strip(), db_recipe[column]))
+        db_recipe[column] = db_recipe[column].str.strip()
     db_recipe.drop("Ing_ID", axis=1, inplace=True)
 
     return db_recipe
@@ -65,52 +68,76 @@ def lims_recipes_to_df(filepath):
     lims_recipe = pd.read_excel(filepath, header=0)
     lims_recipe.columns = cols
     lims_recipe = lims_recipe[cols[:-3]]
-    lims_recipe.dropna(subset=["Ingredient", "Ing_pct"], inplace=True)
+    lims_recipe.dropna(subset=["Ingredient"], inplace=True)
     ingredient_names = lims_recipe["Ingredient"]
     lims_recipe["Function"] = list(map(categorize_ingredient, ingredient_names))
-
+    lims_recipe["Ing_pct"].fillna(0, inplace=True)
 
     return lims_recipe
 
-def concat_tables(lims_table, db_table, print=False, filepath=None):
+def concat_tables(lims_table, db_table, output_file=False, filepath=None):
     concatted = pd.concat([lims_table, db_table], ignore_index=True)
-    if print and filepath:
-        concatted.to_excel(filepath + "full_recipe_list.xlsx", sheet_name="Ingredient_List")
+    concatted["Batch"].fillna("", inplace=True)
+    concatted = concatted.assign(ID=lambda x: x["Request"] + "_" + x["Batch"])
+
+    for column in concatted.columns:
+        if concatted[column].dtype == "object":
+            concatted[column] = concatted[column].str.strip()
+    
+    concatted = concatted.replace(r'\n', ' ', regex=True)
+
+    if output_file and filepath:
+        concatted.to_csv(filepath + full_recipe_list_filename)
+    
+    print(concatted)
+    
     return concatted
 
-def generate_NN_table(dataframe):
+def generate_NN_table(nndf):
     
-    nndf = dataframe[["ID", "Request", "Function", "Ing_pct"]]
-    nndf = nndf.groupby(["ID", "Request", "Function"]).sum().reset_index()
-    nndf = nndf.merge(nndf, how='left', on="Function")
-    nndf.columns = ["ID", "Request", "Function", "Ing_pct", "Comp_ID", "Comp_Request", "Comp_Ing_pct"]
-    nndf = nndf.assign(Delta=lambda x: (x["Ing_pct"] - x["Comp_Ing_pct"]) ** 2)
-    nndf = nndf.groupby(by=["ID", "Comp_ID"]).sum(numeric_only=True).reset_index()
-    nndf = nndf.assign(Distance=lambda x: np.sqrt(x["Delta"])).sort_values(by="Distance")
-    nndf = nndf[["ID", "Comp_ID", "Distance"]]
-    
+    # Ensure each recipe has the same number of ingredients, filling '0' where there is no value.
+    nndf = nndf[["ID", "Function", "Ing_pct"]]
+    nndf = nndf.groupby(["ID", "Function"]).sum().reset_index()
+    nndf = nndf.pivot(index="ID", columns="Function", values="Ing_pct").fillna(0).reset_index()
+    nndf = pd.melt(
+        nndf,
+        id_vars="ID",
+        var_name="Function",
+        value_name="Ing_pct"
+    )
+
+    # Merge datatable with itself and calculate euclidian distance
+    nndf = nndf.merge(nndf, how="left", on="Function", suffixes=(None, "_comp"))
+    nndf = nndf.assign(Delta=lambda x: np.square(x["Ing_pct"] - x["Ing_pct_comp"]))
+    nndf = nndf.groupby(by=["ID", "ID_comp"]).sum(numeric_only=True).reset_index()
+    nndf = nndf.assign(Distance=lambda x: np.sqrt(x["Delta"])).drop("Delta", axis=1)[["ID", "ID_comp", "Distance"]]
+
+    # Export to excel
+    nndf.to_excel(folder_fp + nn_filename, sheet_name="NN_List")
+
     return nndf
 
 def main():
-    folder_fp = env_folder + "files/"
-
-    if Path(folder_fp + old_datatable_output_filename).exists():
-        db_recipe_list = pd.read_excel(folder_fp + old_datatable_output_filename)
+    if Path(folder_fp + full_recipe_list_filename).exists():
+        full_recipe_list = pd.read_csv(folder_fp + full_recipe_list_filename, index_col=0, low_memory=False)
     else:
-        db_recipe_list = database_to_df(folder_fp + old_datatable_filename)
-        db_recipe_list.to_excel(folder_fp + old_datatable_output_filename, index=False)
+        if Path(folder_fp + old_datatable_output_filename).exists():
+            db_recipe_list = pd.read_csv(folder_fp + old_datatable_output_filename)
+        else:
+            db_recipe_list = database_to_df(folder_fp + old_datatable_filename)
+            db_recipe_list.to_csv(folder_fp + old_datatable_output_filename, index=False)
 
-    lims_recipe_list = lims_recipes_to_df(folder_fp + lims_datatable_filename)
+        
+        if Path(folder_fp + lims_recipe_modified).exists():
+            lims_recipe_list = pd.read_csv(folder_fp + lims_recipe_modified)
+        else:
+            lims_recipe_list = lims_recipes_to_df(folder_fp + lims_datatable_filename)
+            lims_recipe_list.to_csv(folder_fp + lims_recipe_modified, index=False)
+
+
+        full_recipe_list = concat_tables(lims_recipe_list, db_recipe_list, output_file=True, filepath=folder_fp)
     
-    if Path(folder_fp + "full_recipe_list.xlsx").exists():
-        full_recipe_list = pd.read_excel(folder_fp + "full_recipe_list.xlsx", index_col=0)
-    else:
-        full_recipe_list = concat_tables(lims_recipe_list, db_recipe_list, print=True, filepath=folder_fp)
-    
-    full_recipe_list = full_recipe_list.assign(ID=lambda x: x["Request"] + "_" + x["Batch"])
-    
-    nndf = generate_NN_table(full_recipe_list)
-    nndf.to_excel(folder_fp + "nearest_neighbor.xlsx", sheet_name="NN_List")
+    generate_NN_table(full_recipe_list)
 
 if __name__ == '__main__':
     main()